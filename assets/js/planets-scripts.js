fe649135932fc4f9a1ee51545406871fdc64ee13
Mehdirps <mehdi.raposo77@gmail.com>
2023-06-22 10:35:55 +0200

    🎨 up : planets filter function

--- assets/js/planets-scripts.js
+++ assets/js/planets-scripts.js
@@ -1,3 +1,10 @@
+async function onInit() {
+    const planetArray = await getPlanets();
+    showPlanets(planetArray);
+    getPlanetDetails(planetArray);
+    getPlanetsListFiltered(planetArray)
+}
+
 function getPlanets() {
     return new Promise(async resolve => {
         let planets = [];
@@ -5,21 +12,45 @@ function getPlanets() {
             const response = await fetch(`https://swapi.dev/api/planets/?page=${i}`);
             const data = await response.json();
             planets = planets.concat(data.results);
-
         }
         resolve(planets);
     });
 }
 
-async function getPlanetsList() {
-    const planetArray = await getPlanets();
-    return planetArray;
+async function getPlanetsListFiltered(planets) {
+
+    document.querySelector('#planete').addEventListener('change', (e) => {
+
+        let planetsFiltered;
+        const value = e.target.value;
+
+        switch (value) {
+            case "1":
+                planetsFiltered = planets.filter(item => item.population <= 100000);
+                break;
+            case "2":
+                planetsFiltered = planets.filter(item => item.population >= 100000 && item.population <= 100000000);
+                break;
+            case "3":
+                planetsFiltered = planets.filter(item => item.population >= 100000 && item.population >= 100000000);
+                break;
+            case "all":
+                planetsFiltered = planets;
+                break;
+        }
+        showPlanets(planetsFiltered);
+        getPlanetDetails(planetsFiltered);
+    });
+
 }
 
-async function showPlanet() {
-    const planets = await getPlanetsList();
+async function showPlanets(planets) {
+
 
     const arrayBody = document.querySelector('.array-body');
+
+    arrayBody.innerHTML = "";
+
     for (let planet of planets) {
         const name = planet.name;
         const nameParagraph = document.createElement('p');
@@ -41,22 +72,23 @@ async function showPlanet() {
         arrayItem.appendChild(zoneParagraph);
 
         arrayBody.appendChild(arrayItem);
-
     }
+    getPlanetDetails(planets);
     document.querySelector('.item-number .number').textContent = planets.length;
 }
 
-showPlanet();
-
-async function getPlanetDetails() {
-    const planets = await getPlanetsList();
+async function getPlanetDetails(planets) {
 
     const planetList = document.querySelectorAll('.array-item');
 
     for (let planet of planetList) {
+
         planet.addEventListener('click', (e) => {
+
             let planetName = e.target.attributes['name'].value;
+
             const planetFiltered = planets.filter(item => item.name === planetName);
+            
             document.querySelector('.planet-details .name').textContent = planetFiltered[0].name;
             document.querySelector('.population-number').textContent = planetFiltered[0].population;
             document.querySelector('.size').textContent = planetFiltered[0].diameter;
@@ -65,8 +97,6 @@ async function getPlanetDetails() {
             document.querySelector('.planet-infos .zone').textContent = planetFiltered[0].terrain
         });
     }
-
 }
 
-getPlanetDetails();
-
+onInit();
